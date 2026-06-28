c0d6cbe6d35656e9e253213713e2773aff5fa10c
Hitesh Mistry <cancer32@rocketmail.com>
2026-06-28 11:49:19 +0530

    Working changes

--- bin/env-manager.bat
+++ bin/env-manager.bat
@@ -6,5 +6,5 @@ set PATH=%BIN_DIR%;%PATH%
 REM Check if CONDA_ROOT is already set
 call _set_env.bat
 
-%CONDA_ROOT%/python.exe %BIN_DIR%/../env_manager_main.py %*
+python %BIN_DIR%/../env_manager_main.py %*
 exit /b %ERRORLEVEL%
